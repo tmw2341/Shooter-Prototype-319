a7b387060fcdfded64d238f6a2964ad62afcd5ff
Tyler Witte <tylerwitte44@msn.com>
2017-10-11 21:07:11 -0500

    Further Progress on the Game Functionality

--- CS-319-Portofolio1/CS-319-Portofolio1/js/game.js
+++ CS-319-Portofolio1/CS-319-Portofolio1/js/game.js
@@ -2,10 +2,12 @@
 function startGame() {
     init();
     myGameArea.start();
-    myGamePiece = new component(10, 10, "red", 30, 30);
+    myGameArea.pieces.push(new component(30, 30, "red", 30, 30));
+    myGameArea.pieces.push(new component(30, 30, "red", 60, 30));
 }
 
 var myGameArea = {
+    pieces: [],
     canvas: null,
     start: function () {
         this.context = this.canvas.getContext('2d');
@@ -13,17 +15,21 @@ var myGameArea = {
         window.addEventListener('mousedown', function (e) {
             myGameArea.move = true;
             myGameArea.x = e.pageX;
-            myGameArea.y = 
+            myGameArea.y = e.pageY;
         })
-
         window.addEventListener('mouseup', function (e) {
-            window.removeEventListener('mousemove', e);
             myGameArea.move = false;
+            myGameArea.x = null;
+            myGameArea.y = null;
+            myGameArea.newX = null;
+            myGameArea.newY = null;
         })
         window.addEventListener('mousemove', function (e) {
-            if (myGameArea.move) {
-                myGameArea.x = e.pageX;
-                myGameArea.y = e.pageY;
+            for (i = 0; i < myGameArea.pieces.length; ++i){
+                if (myGameArea.move && myGameArea.pieces[i].clicked()) {
+                    myGameArea.newX = e.pageX;
+                    myGameArea.newY = e.pageY;
+                } 
             }
         })
     },
@@ -54,13 +60,13 @@ function component(width, height, color, x, y) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     this.clicked = function () {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
+        var myleft = this.x + (document.documentElement.clientWidth * 0.02);
+        var myright = this.x + ((this.width) + document.documentElement.clientWidth * 0.02);
+        var mytop = this.y + (document.documentElement.clientHeight * 0.02);
+        var mybottom = this.y + ((this.height) + document.documentElement.clientHeight * 0.02);
         var clicked = false;
-        if ((mybottom > myGameArea.y) || (mytop < myGameArea.y)
-            || (myright > myGameArea.x) || (myleft < myGameArea.x)) {
+        if ((mybottom > myGameArea.y) && (mytop < myGameArea.y)
+            && (myright > myGameArea.x) && (myleft < myGameArea.x)) {
             clicked = true;
         }
         return clicked;
@@ -69,34 +75,42 @@ function component(width, height, color, x, y) {
 
 function updateGameArea() {
     myGameArea.clear();
-    if (myGameArea.x && myGameArea.y) {
-        myGamePiece.x = myGameArea.x;
-        myGamePiece.y = myGameArea.y;
+    for (i = 0; i < myGameArea.pieces.length; ++i){
+        if (myGameArea.x && myGameArea.y && myGameArea.newX && myGameArea.newY) {
+            myGameArea.pieces[i].x -= myGameArea.x - myGameArea.newX;
+            myGameArea.pieces[i].y -= myGameArea.y - myGameArea.newY;
+            myGameArea.x = myGameArea.newX;
+            myGameArea.y = myGameArea.newY;
+        }
+        if (myGameArea.pieces[i].clicked()) {
+            myGameArea.pieces[i].color = "yellow";
+            myGameArea.pieces[i].newPos();
+            myGameArea.pieces[i].update();
+        } else {
+            myGameArea.pieces[i].color = "red";
+        }
+        myGameArea.pieces[i].newPos();
+        myGameArea.pieces[i].update();
     }
-    if (myGamePiece.clicked) {
-        myGamePiece.color = "yellow";
-    }
-    myGamePiece.newPos();
-    myGamePiece.update();
 }
 
 function moveup() {
-    myGamePiece.speedY -= 1;
+    myGameArea.pieces[1].speedY -= 1;
 }
 
 function movedown() {
-    myGamePiece.speedY += 1;
+    myGameArea.pieces[1].speedY += 1;
 }
 
 function moveleft() {
-    myGamePiece.speedX -= 1;
+    myGameArea.pieces[1].speedX -= 1;
 }
 
 function moveright() {
-    myGamePiece.speedX += 1;
+    myGameArea.pieces[1].speedX += 1;
 }
     
 function stopMove() {
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
+    myGameArea.pieces[1].speedX = 0;
+    myGameArea.pieces[1].speedY = 0;
 }
