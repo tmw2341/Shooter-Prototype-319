900007dc13357a75f8f7347ec74619d8f040ebe4
Alex! <ikeos2@gmail.com>
2017-10-16 18:46:28 -0500

    Auto stash before merge of "master" and "origin/master"

--- Server/server.py
+++ Server/server.py
@@ -24,19 +24,27 @@ def pull():
 # Creates a new unit, appends to units array
 @app.route('/new_unit', methods=['POST'])
 def new_unt():
-    data = request.args
+    data = request.values
+
+    #import pdb;pdb.set_trace()
 
     global units
 
-    if data['owner'] and data['posx'] and data['posy'] and data['unit_id']:
-        unit = {}
-        unit['owner'] = data['owner']
-        unit['posx'] = data['posx']
-        unit['posy'] = data['posy']
-        unit['unit_id'] = data['unit_id']
-        units.append(unit);
-        return "success"
-    return "fail"
+    try:
+        if data['owner'] and data['posx'] and data['posy'] and data['unit_id'] and data['type']:
+            unit = {}
+            unit['owner'] = data['owner']
+            unit['posx'] = data['posx']
+            unit['posy'] = data['posy']
+            unit['unit_id'] = data['unit_id']
+            unit['type'] = data['type']
+            units.append(unit);
+            return "success"
+    except KeyError:
+        return "Missing keys!"
+
+
+    return "Something went terribly wrong"
 
 
 # Updates the position of an existing unit
@@ -54,4 +62,21 @@ def push():
                 units[x]['posx'] = int(data['posx']);
                 units[x]['posy'] = int(data['posy']);
                 return 'success';
+    return "fail";
+
+# removes an existing unit
+@app.route('/remove', methods=['POST'])
+def remove():
+    # print "data: " + str(request.args);
+    data = request.args
+    # dataDict = json.loads(data)
+
+    global units
+
+    if data['unit_id']:
+        for x in range(0, len(units)):
+            if data['unit_id'] == units[x]['unit_id']:
+                units[x] = None
+                return 'success'
+        return "Unit not found"
     return "fail";
